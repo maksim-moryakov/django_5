35282af11e5cb2cf3e5268d955592ff8a1e5c5bd
Maksim Moryakov <79351987+maksim-moryakov@users.noreply.github.com>
2024-06-06 18:12:47 +0300

    добавили app к проекту и дополнили модель поста

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,11 +1,38 @@
 from django.db import models
+from django.utils import timezone
 
-# структура поста: заголовок (250 символов); краткое описание (250 символов); основной текст
+# структура поста:
 
 class Post(models.Model):
+    # класс создан для сохранения сокращения статуса поста: опубликован или драфт
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+    # поле заголовок поста, ограничение 250 символов
     title = models.CharField(max_length=250)
+    # поле краткое описание постав
     slug = models.SlugField(max_length=250)
+    # поле основная часть поста
     body = models.TextField()
+    # поле дата поста, по дефолту текущее время пользователя
+    publish = models.DateTimeField(default=timezone.now)
+    # поле дата создания поста, дата время будет сохранятся
+    created = models.DateTimeField(auto_now_add=True)
+    # поле храненения даты и времени обнавления поста: дата атоматически будет обновляться
+    updated = models.DateTimeField(auto_now=True)
+    # поле статуса поста: подефолту черновик. Тянет значения из класса Status
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT
+    )
 
+    # данный класс создан для сортировки постов по полю даты создания поста
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+    
     def __str__(self) -> str:
         return self.title
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogCofig', # активация приложения blog
 ]
 
 MIDDLEWARE = [
